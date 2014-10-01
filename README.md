7774340c261beba3c3236180afbc1782b27bf1cd
Jewel Ahmed <tojibon@gmail.com>
2014-10-01 14:19:12 -0700

    jQuery VOpacity plugins added

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ Then, When you scroll down your page and it's visible area are going less then i
 
 Usages:
 ===============
+<code>
 jQuery('div.class').vopacity(0.15, true);
 jQuery('.class').vopacity(0.15, true);
 jQuery('div').vopacity(0.15, true);
+</code>
 
 Where first arg 0.15 is first opacity down value that will be applied when the container first time going to apply it's calculated opacity.
 The send arg is a bool one, you can use true to use container outerHeight(true) as it's height otherwise it will use height() to calculate it's height.
@@ -20,6 +22,7 @@ You can use vopacity on a container and mention it's another container to apply 
 
 Plugins Used Container:
 ==============================
+<code>
 <div class="vopacity" data-content-dynamic-opacity-class="vopacity-inner-content">
 	PADDING-TOP = 100PX;
 	<div class="vopacity-inner-content">
@@ -30,3 +33,4 @@ Plugins Used Container:
 <script>
 	jQuery('.vopacity').vopacity(0.15, true);
 </script>
+</code>
---
+++ jquery.vopacity.js
@@ -0,0 +1,76 @@
+/*
+---------------------------------------------------------------------------------------
+    Variable / Dynamic Content Opacity depends on page scroll or page down
+    Plugin: jQuery Vopacity
+	Version 1.0.0
+	Author: Jewel Ahmed
+	Author URL: http://www.codeatomic.com/
+
+	Licensed under MIT and GPLv2.
+	Usage: jQuery('div.class').vopacity(0.15, true);
+---------------------------------------------------------------------------------------
+*/
+(function( $ ){
+	var $window = $(window);
+	var windowHeight = $window.height();
+	var windowWidth = $window.width();
+	
+	$window.resize(function () {
+		windowHeight = $window.height();
+		windowWidth = $window.width();
+	});
+
+	$.fn.vopacity = function( extraDownSpeed, outerHeight ) {
+		var $this = $(this);
+		var getHeight;
+		var paddingTop = 0;
+		
+		
+		if (outerHeight) {
+			getHeight = function(jqo) {
+				return jqo.outerHeight(true);
+			};
+		} else {
+			getHeight = function(jqo) {
+				return jqo.height();
+			};
+		}
+			
+		if (arguments.length < 1 || extraDownSpeed === null) extraDownSpeed = 0;
+		if (arguments.length < 2 || outerHeight === null) outerHeight = true;
+		
+		function update(){
+			var windowScrollTop = $window.scrollTop();				
+			
+			$this.each(function(){
+				var $element = $(this);
+				var top = $element.offset().top;
+				var height = getHeight($element);
+	
+				if (top + height < windowScrollTop) { //Hidden at screen top area / just above of the viewport
+					return;
+				} else if (top > windowScrollTop + windowHeight) { //Hidden at screen bottom area / just below of the viewport
+					return;
+				}
+				
+				var $visibleHeight = ( ( top + height ) - windowScrollTop );
+				if ( $visibleHeight > height ) {
+					$visibleHeight = height;
+				}
+				
+				var $calculatedOpacity = ( 1 / height ) * $visibleHeight;
+				if ( $calculatedOpacity < 1 && extraDownSpeed > 0 ) {
+					$calculatedOpacity-= extraDownSpeed;
+				}
+				if ( typeof $(this).data('content-dynamic-opacity-class') !== 'undefined' && $(this).data('content-dynamic-opacity-class') != '') {
+					$element.find('.'+$(this).data('content-dynamic-opacity-class')).stop(true,true).animate( { 'opacity' : $calculatedOpacity }, 0, 'swing' );			
+				} else {
+					$element.stop(true,true).animate( { 'opacity' : $calculatedOpacity }, 0, 'swing' );
+				}
+			});
+		}	
+
+		$window.bind('scroll', update).resize(update);
+		update();
+	};
+})(jQuery);
